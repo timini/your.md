4de1f3cb19c28d82c55054d30e94057bbe340098
Tim Richardson <tim.richardson@congenica.com>
2018-05-10 22:57:06 +0100

    better comments

--- src/matchPhrases.js
+++ src/matchPhrases.js
@@ -2,11 +2,13 @@ const escapeRegExp = (str) =>
   str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
 
 const phraseMatcher = (phrases = [], text = '') => {
+  if (!Array.isArray(phrases)) throw new Error('phrases must be an array of strings');
   if (typeof text !== 'string') throw new Error('text must be a string');
-  if (!Array.isArray(phrases)) throw new Error('createMatcher takes an array of strings');
 
   return phrases.reduce((acc, phrase) => {
+    // skip empty phrases
     if (phrase === '') return acc;
+    // check text for phrase
     if (RegExp(escapeRegExp(phrase), 'g').test(text)) acc.push(phrase);
     return acc;
   }, [])
